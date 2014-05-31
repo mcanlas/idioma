f33538ea7b48e1efb992f5978b55142d405c1535
Mark Canlas <github@htmlism.com>
2014-05-31 06:25:36 -0400

    added code generator

---
+++ grammar/src/main/scala/com/htmlism/idioma/GenerateConjugationCode.scala
@@ -0,0 +1,22 @@
+package com.htmlism.idioma
+
+object GenerateConjugationCode extends App {
+  val persons = List("first", "second", "third")
+  val numbers = List("Singular", "Plural")
+  val tenses = List("Present", "Perfect", "Imperfect", "Pluperfect", "Future", "Conditional", "PresentSubjunctive", "ImperfectSubjunctive", "FutureSubjunctive")
+
+  for (tense <- tenses) {
+    println(s"\n// $tense")
+
+    val valueNames = for {
+      number <- numbers
+      person <- persons
+    } yield s"${person}Person$number$tense"
+
+    val maximumLength = valueNames.map { _.length }.max
+
+    for (v <- valueNames) {
+      printf(s"val %-${maximumLength}s = root + ???\n", v)
+    }
+  }
+}
