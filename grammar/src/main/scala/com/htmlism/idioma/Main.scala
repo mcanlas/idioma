2ffb22a3aa467185697f6cef4b35f74f1d2f46a3
Mark Canlas <github@htmlism.com>
2014-06-02 18:56:44 -0400

    made verbs portuguese specific

--- grammar/src/main/scala/com/htmlism/idioma/Main.scala
+++ grammar/src/main/scala/com/htmlism/idioma/Main.scala
@@ -6,7 +6,7 @@ import org.json4s.native.JsonMethods._
 object Main extends App {
   val input = org.json4s.file2JsonInput(new java.io.File("verbs.json"))
 
-  val json = parse(new java.io.File("verbs.json"))
+  val json = parse(new java.io.File("data/portuguese/verbs.json"))
 
   for (JArray(verbs) <- json) {
     for (json <- verbs) {
