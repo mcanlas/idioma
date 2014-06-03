4761013082b44b0c81ec09cebe4db9f19fad0b6e
Mark Canlas <github@htmlism.com>
2014-06-02 20:00:16 -0400

    added codes

--- portuguese/src/main/scala/com/htmlism/idioma/portuguese/GrammaticalCategories.scala
+++ portuguese/src/main/scala/com/htmlism/idioma/portuguese/GrammaticalCategories.scala
@@ -1,31 +1,31 @@
 package com.htmlism.idioma.portuguese
 
 object GrammaticalCategories {
-  val Present     = Tense("present")
-  val Perfect     = Tense("perfect")
-  val Imperfect   = Tense("imperfect")
-  val Pluperfect  = Tense("pluperfect")
-  val Future      = Tense("future")
-  val Conditional = Tense("conditional")
+  val Present     = Tense("present",     "PRES")
+  val Perfect     = Tense("perfect",     "PERF")
+  val Imperfect   = Tense("imperfect",   "IMP")
+  val Pluperfect  = Tense("pluperfect",  "PLU")
+  val Future      = Tense("future",      "FUT")
+  val Conditional = Tense("conditional", "CND")
 
-  val PresentSubjunctive   = Tense("presentSubjunctive")
-  val ImperfectSubjunctive = Tense("imperfectSubjunctive")
-  val FutureSubjunctive    = Tense("futureSubjunctive")
+  val PresentSubjunctive   = Tense("presentSubjunctive",   "PRESSUB")
+  val ImperfectSubjunctive = Tense("imperfectSubjunctive", "IMPSUB")
+  val FutureSubjunctive    = Tense("futureSubjunctive",    "FUTSUB")
 
-  val FirstPerson  = Person("first")
-  val SecondPerson = Person("second")
-  val ThirdPerson  = Person("third")
+  val FirstPerson  = Person("first", "1")
+  val SecondPerson = Person("second", "2")
+  val ThirdPerson  = Person("third", "3")
 
-  val Singular  = Number("singular")
-  val Plural    = Number("plural")
+  val Singular  = Number("singular", "S")
+  val Plural    = Number("plural", "P")
 
   val Tenses  = List(Present, Perfect, Imperfect, Pluperfect, Future, Conditional, PresentSubjunctive, ImperfectSubjunctive, FutureSubjunctive)
   val Persons = List(FirstPerson, SecondPerson, ThirdPerson)
   val Numbers = List(Singular, Plural)
 }
 
-case class Tense(tense: String)
+case class Tense(tense: String, code: String)
 
-case class Person(person: String)
+case class Person(person: String, code: String)
 
-case class Number(number: String)
+case class Number(number: String, code: String)
