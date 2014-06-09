7359dd9aa12ad6c0066d49646328a2b090c3689c
Mark Canlas <github@htmlism.com>
2014-06-09 00:05:26 -0400

    added gerund generation

--- portuguese/src/main/scala/com/htmlism/idioma/portuguese/Main.scala
+++ portuguese/src/main/scala/com/htmlism/idioma/portuguese/Main.scala
@@ -15,7 +15,7 @@ object Main extends App {
     case _ => throw new IllegalArgumentException
   }
 
-  val sentences = verbs.flatMap { verb =>
+  val verbalPhrases = verbs.flatMap { verb =>
     List(Present, Perfect, Imperfect).flatMap { t =>
       val timePhrases = t match {
         case Present   => Nil :: Nil
@@ -54,6 +54,43 @@ object Main extends App {
       }
     }
   }
+
+  val progressivePhrases = {
+    val copula = verbs.filter{ v => v.infinitive == "estar" }.head
+
+    verbs.flatMap { verb =>
+      val phrases = List(Present, Imperfect).flatMap { t =>
+        Persons.flatMap { p =>
+          Numbers.flatMap { n =>
+            val form = copula(t, p, n).word
+
+            (p, n) match {
+              case (FirstPerson, Singular) =>
+                List("eu", form) :: Nil
+              case (ThirdPerson, Singular) =>
+                List("você", form) ::
+                  List("ele", form) ::
+                  List("ela", form) ::
+                  List("a", "gente", form) ::
+                  Nil
+              case (FirstPerson, Plural) =>
+                List("nós", form) :: Nil
+              case (ThirdPerson, Plural) =>
+                List("vocês", form) ::
+                  List("eles", form) ::
+                  List("elas", form) :: Nil
+              case _ =>
+                Nil
+            }
+          }
+        }
+      }
+
+      phrases.map { p => p :+ verb.gerund }
+    }
+  }
+
+  val sentences = verbalPhrases ++ progressivePhrases
 
   for (s <- sentences) printSentence(s)
 
