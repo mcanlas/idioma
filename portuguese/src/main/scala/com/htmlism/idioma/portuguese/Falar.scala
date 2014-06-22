3ad4e07b895ab239152ab4adbcf78780fc6fe0ed
Mark Canlas <github@htmlism.com>
2014-06-22 02:13:09 -0400

    added conjugation

--- portuguese/src/main/scala/com/htmlism/idioma/portuguese/Falar.scala
+++ portuguese/src/main/scala/com/htmlism/idioma/portuguese/Falar.scala
@@ -7,5 +7,22 @@ object Falar extends App {
   val verbForms = Generator(Seq(Singular, Plural)) + Generator(Seq(FirstPerson, ThirdPerson))
   val tenses    = Generator(Seq('present, 'perfect, 'imperfect, 'future, 'presentProgressive, 'pastProgressive))
 
-  (tenses + verbForms + Data.idiomas).iterator.foreach { println }
+  val verb      = Data.verbs.filter{ v => v.infinitive == "falar" }.head
+  val copula    = Data.verbs.filter{ v => v.infinitive == "estar" }.head
+  val auxiliary = Data.verbs.filter{ v => v.infinitive == "ir" }.head
+
+  def conjugate(tense: Symbol, number: Number, person: Person) = tense match {
+    case 'present   => Phrase(verb(Present, person, number).word)
+    case 'perfect   => Phrase(verb(Perfect, person, number).word)
+    case 'imperfect => Phrase(verb(Imperfect, person, number).word)
+    case 'future => Phrase.empty
+    case 'presentProgressive => Phrase.empty
+    case 'pastProgressive => Phrase.empty
+  }
+
+  (tenses + verbForms + Data.idiomas).iterator.map { case ((tense, (number, person)), language) =>
+    val verb = conjugate(tense, number, person)
+
+    verb + language
+  }.foreach { p => println(new Sentence(p.words).render) }
 }
