0ff99b7e699d277bbe4549aee1986c1301ab7865
Mark Canlas <github@htmlism.com>
2014-09-08 23:11:56 -0400

    added lesson 3 michel thomas arabic

---
+++ arabic/src/main/scala/com/htmlism/idioma/arabic/Lesson3.scala
@@ -0,0 +1,11 @@
+package com.htmlism.idioma.arabic
+
+object Lesson3 {
+  val englishWords = Seq("bank", "film", "CD", "cola", "cinema")
+
+  val loanWords = Seq("sandawich", "bitsa", "subermarket", "telephone", "doctor", "radio")
+
+  val nativeWords = Seq("kebab", "falafel", "bazaar", "souk", "gamel")
+
+  val lessObviousWords = Seq("sukar", "chay")
+}
