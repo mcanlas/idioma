e2eeca981b67c5788606d1b502a1ff4b81e13db4
Mark Canlas <github@htmlism.com>
2014-09-09 18:46:38 -0400

    added lesson 4

---
+++ arabic/src/main/scala/com/htmlism/idioma/arabic/Lesson4.scala
@@ -0,0 +1,18 @@
+package com.htmlism.idioma.arabic
+
+import com.htmlism.idioma._
+
+object Lesson4 extends App {
+  val mayIHave = "mumkin"
+  val please = Seq("min", "fadlak")
+
+  val foods = Seq("cola", "sandawich", "bitsa", "kebab", "falafel", "shay")
+
+  for (f <- foods) {
+    println(new Sentence(Seq(mayIHave, f)).render)
+  }
+
+  for (f <- foods) {
+    println(new Sentence(Seq(mayIHave, f) ++ please).render)
+  }
+}
