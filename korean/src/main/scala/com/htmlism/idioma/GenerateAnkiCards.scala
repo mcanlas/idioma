98fadc2163111ee21809968dc0bacbf9ec17752f
Mark Canlas <github@htmlism.com>
2015-11-11 01:10:47 -0500

    emit cards

--- korean/src/main/scala/com/htmlism/idioma/GenerateAnkiCards.scala
+++ korean/src/main/scala/com/htmlism/idioma/GenerateAnkiCards.scala
@@ -15,7 +15,14 @@ object GenerateAnkiCards extends App {
   // skip the header
   consonants.next()
 
-  consonants.foreach(out.println)
+  consonants
+    .map { s =>
+      val Array(n, name) = s.split(",")
+
+      AnkiCardValue(s"What is the name of this consonant?<div>$n</div>", name)
+    }
+    .map(_.serialize)
+    .foreach(out.println)
 
   out.close()
 }
@@ -24,6 +31,8 @@ trait AnkiCard {
   def front: String
 
   def back: String
+
+  def serialize: String = front + "\t" + back
 }
 
 case class AnkiCardValue(front: String, back: String) extends AnkiCard
