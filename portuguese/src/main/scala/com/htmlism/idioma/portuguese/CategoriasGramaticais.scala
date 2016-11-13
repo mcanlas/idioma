8d939f0db55a530446e4d21b0d166fc20237a1ac
Mark Canlas <github@htmlism.com>
2016-11-12 17:15:58 -0800

    relax type

--- portuguese/src/main/scala/com/htmlism/idioma/portuguese/CategoriasGramaticais.scala
+++ portuguese/src/main/scala/com/htmlism/idioma/portuguese/CategoriasGramaticais.scala
@@ -4,10 +4,10 @@ object CategoriasGramaticais {
   val Definitivo   = Especificação("definite",   "d")
   val Indefinitivo = Especificação("indefinite", "i")
 
-  val Tempos  = List(Presente, Perfeito, Imperfeito, MasQuePerfeito, Futuro, Condicional, SubjuntivoDoPresente, SubjuntivoDoImperfeito, SubjuntivoDoFuturo)
-  val Pessoas = List(PessoaPrimeira, PessoaSegunda, PessoaTerceira)
-  val Numeros = List(Singular, Plural)
-  val Generos = List(Masculino, Feminino, Ambos)
+  val Tempos  = Seq(Presente, Perfeito, Imperfeito, MasQuePerfeito, Futuro, Condicional, SubjuntivoDoPresente, SubjuntivoDoImperfeito, SubjuntivoDoFuturo)
+  val Pessoas = Seq(PessoaPrimeira, PessoaSegunda, PessoaTerceira)
+  val Numeros = Seq(Singular, Plural)
+  val Generos = Seq(Masculino, Feminino, Ambos)
 
   val Pronomes = Map(
     (Singular, PessoaPrimeira) -> Seq("eu"),
