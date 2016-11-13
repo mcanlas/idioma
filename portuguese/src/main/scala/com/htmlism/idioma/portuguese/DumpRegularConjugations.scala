1470548afea81a4f726f099cd47f8ba518a28b7a
Mark Canlas <github@htmlism.com>
2016-11-13 14:21:14 -0800

    added working demonstration

---
+++ portuguese/src/main/scala/com/htmlism/idioma/portuguese/DumpRegularConjugations.scala
@@ -0,0 +1,26 @@
+package com.htmlism.idioma.portuguese
+
+object DumpRegularConjugations extends App {
+  val verbs = Seq(
+    "fal" -> resources.firstConjugation,
+    "com" -> resources.secondConjugation,
+    "assist" -> resources.thirdConjugation)
+
+  for ((root, conjugation) <- verbs) {
+    println
+
+    val columns = for (t <- Seq(Presente, Perfeito, Imperfeito)) yield {
+      val inflections = for (n <- CategoriasGramaticais.Numeros; p <- CategoriasGramaticais.Pessoas) yield
+        conjugation(root, (t, p, n)).word
+
+      t.key +: inflections
+    }
+
+    val lines =
+      Padder.zip(
+        Padder.zip(columns(0), columns(1)),
+        columns(2))
+
+    lines.foreach(println)
+  }
+}
