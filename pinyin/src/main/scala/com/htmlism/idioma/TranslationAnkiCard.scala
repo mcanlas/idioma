41360dea120022469f5e713a8c136acbef1615fc
Mark Canlas <github@htmlism.com>
2017-02-12 19:38:03 -0500

    added deck generator

---
+++ pinyin/src/main/scala/com/htmlism/idioma/TranslationAnkiCard.scala
@@ -0,0 +1,60 @@
+package com.htmlism.idioma
+
+trait TranslationAnkiCard extends AnkiCard {
+  def id = (getClass.getName.hashCode + word.hashCode).toString
+
+  def word: ChineseWord
+}
+
+case class EnglishToChinese(word: ChineseWord) extends TranslationAnkiCard {
+  def front: String = ???
+
+  def back: String = ???
+}
+
+case class ChineseToEnglish(word: ChineseWord) extends TranslationAnkiCard {
+  def front: String =
+    s"""<div id="preface">What does this mean?</div>""" +
+      s"""<div id="heroic-prompt-korean">${word.chinese}</div>"""
+
+  def back: String =
+    s"""<div id="heroic-prompt-english">${word.english}</div>"""
+}
+
+case class ChineseToPinyin(word: ChineseWord) extends TranslationAnkiCard {
+  def front: String =
+    s"""<div id="preface">How do you pronounce this?</div>""" +
+      s"""<div id="heroic-prompt-korean">${word.chinese}</div>"""
+
+  def back: String =
+    s"""<div id="heroic-prompt-korean">${word.pinyin}</div>"""
+}
+
+case class PinyinToChinese(word: ChineseWord) extends TranslationAnkiCard {
+  def front: String =
+    s"""<div id="preface">How do you write this in Chinese?</div>""" +
+      s"""<div id="heroic-prompt-korean">${word.pinyin}</div>"""
+
+  def back: String =
+    s"""<div id="heroic-prompt-korean">${word.chinese}</div>"""
+}
+
+case class EnglishToPinyin(word: ChineseWord) extends TranslationAnkiCard {
+  def front: String =
+    s"""<div id="preface">How do you write this in Pinyin?</div>""" +
+      s"""<div id="heroic-prompt-english">${word.english}</div>"""
+
+
+  def back: String =
+    s"""<div id="heroic-prompt-korean">${word.pinyin}</div>"""
+}
+
+case class PinyinToEnglish(word: ChineseWord) extends TranslationAnkiCard {
+  def front: String =
+    s"""<div id="preface">What does this mean?</div>""" +
+    s"""<div id="heroic-prompt-korean">${word.pinyin}</div>"""
+
+
+  def back: String =
+    s"""<div id="heroic-prompt-english">${word.english}</div>"""
+}
