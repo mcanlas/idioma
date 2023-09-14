ba5115d2529705d2ea08b8e0b4e672d5287aaf86
Mark Canlas <github@htmlism.com>
2023-09-14 12:19:23 +0200

    unchecked

--- portuguese/src/main/scala/com/htmlism/idioma/portuguese/OldVerb.scala
+++ portuguese/src/main/scala/com/htmlism/idioma/portuguese/OldVerb.scala
@@ -11,8 +11,7 @@ object OldVerb:
       case JObject(fields) =>
         val map = fields.toMap
 
-        @unchecked
-        val JString(infinitive) = map("infinitive")
+        val JString(infinitive) = map("infinitive"): @unchecked
 
         val (root, conjugation) = Conjugação(infinitive)
           .getOrElse(throw new RuntimeException(s"could not conjugate infinitive $infinitive"))
