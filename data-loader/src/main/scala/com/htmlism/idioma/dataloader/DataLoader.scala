13a31bf7f23b2ada8e3d4ba7bdbbe7a6f544e86f
Mark Canlas <github@htmlism.com>
2023-09-14 10:49:27 +0200

    slurp

--- data-loader/src/main/scala/com/htmlism/idioma/dataloader/DataLoader.scala
+++ data-loader/src/main/scala/com/htmlism/idioma/dataloader/DataLoader.scala
@@ -11,26 +11,12 @@ import io.circe.parser.decode
 
 object DataLoader:
   def getJson[F[_], A: JsonDecoder](is: => InputStream)(using F: Sync[F]): F[A] =
-    Resource
-      .fromAutoCloseable(F.delay(is))
-      .use { is =>
-        F.delay:
-          Source
-            .fromInputStream(is)
-            .mkString
-      }
+    slurp(is)
       .map(decode[A](_)(using summon[JsonDecoder[A]].circeDecoder))
       .flatMap(_.liftTo[F])
 
   def getYaml[F[_], A: JsonDecoder](is: => InputStream)(using F: Sync[F]): F[A] =
-    Resource
-      .fromAutoCloseable(F.delay(is))
-      .use { is =>
-        F.delay:
-          Source
-            .fromInputStream(is)
-            .mkString
-      }
+    slurp(is)
       .map(io.circe.yaml.parser.parse)
       .map(_.flatMap(_.as[A](using summon[JsonDecoder[A]].circeDecoder)))
       .flatMap(_.liftTo[F])
@@ -38,3 +24,13 @@ object DataLoader:
   def getJsonUnsafe[A: JsonDecoder](is: => InputStream): A =
     getJson[IO, A](is)
       .unsafeRunSync()(using cats.effect.unsafe.implicits.global)
+
+  private def slurp[F[_]](is: => InputStream)(using F: Sync[F]): F[String] =
+    Resource
+      .fromAutoCloseable(F.delay(is))
+      .use { is =>
+        F.delay:
+          Source
+            .fromInputStream(is)
+            .mkString
+      }
