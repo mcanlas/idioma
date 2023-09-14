2adf6b0df943f5290181fe79fb10ee426a38be38
Mark Canlas <github@htmlism.com>
2023-09-14 11:28:36 +0200

    verbs

--- korean/src/main/scala/com/htmlism/idioma/VerbCards.scala
+++ korean/src/main/scala/com/htmlism/idioma/VerbCards.scala
@@ -1,28 +1,20 @@
 package com.htmlism.idioma
 
-import scala.io.Source
-
-import org.json4s._
-import org.json4s.native.JsonMethods._
+import com.htmlism.idioma._
+import com.htmlism.idioma.dataloader._
 
 object VerbCards extends TranslationCardIterator:
   def partsIterator: Iterator[(String, String, String)] =
-    val verbs = Source
-      .fromInputStream(getClass.getResourceAsStream("/verbs.json"))
-      .mkString
-
-    // : Iterable[(String, JValue)]
-
-    val nouns = parse(verbs) match
-      case JObject(jsValues) => jsValues
-      case _                 => Nil
-
-    nouns
-      .iterator
-      .map { case (english, json) =>
-        implicit val formats = DefaultFormats
-
-        val korean = (json \ "korean").extract[String]
-
-        (english, "to " + english, korean)
+    DataLoader
+      .getJsonUnsafe[Map[String, Verb]](getClass.getResourceAsStream("/verbs.json"))
+      .toList
+      .map { case (eng, verb) =>
+        (eng, "to " + eng, verb.korean)
       }
+      .iterator
+
+case class Verb(korean: String)
+
+object Verb:
+  given JsonDecoder[Verb] =
+    JsonDecoder.derive
