f20aa4ee4f9ce0d58945c30959d01533948eb76a
Mark Canlas <github@htmlism.com>
2024-05-13 13:50:05 -0400

    add missing codec since circe upgrade

--- portuguese/src/main/scala/com/htmlism/idioma/portuguese/Genero.scala
+++ portuguese/src/main/scala/com/htmlism/idioma/portuguese/Genero.scala
@@ -1,6 +1,13 @@
 package com.htmlism.idioma.portuguese
 
+import io.circe.Decoder
+import io.circe.generic.semiauto.deriveDecoder
+
 enum Gender:
   case Masculine
   case Feminine
   case Neuter
+
+object Gender:
+  given Decoder[Gender] =
+    deriveDecoder
