4c4499e839f1db9fe527e8fd2a425e6924626129
Mark Canlas <github@htmlism.com>
2026-03-13 21:35:18 -0400

    dependencies :dna:

--- project/DependenciesPlugin.scala
+++ project/DependenciesPlugin.scala
@@ -9,6 +9,10 @@ object DependenciesPlugin extends AutoPlugin {
       val circeVersion =
         "0.14.10"
 
+      def withCats: Project =
+        p
+          .settings(libraryDependencies += "org.typelevel" %% "cats-core" % "2.13.0")
+
       def withYaml: Project =
         p.settings(
           libraryDependencies ++= Seq(
@@ -27,6 +31,18 @@ object DependenciesPlugin extends AutoPlugin {
       def withEffectMonad: Project =
         p
           .settings(libraryDependencies += "org.typelevel" %% "cats-effect" % "3.6.3")
+
+      def withTesting: Project = {
+        val weaverVersion =
+          "0.8.4"
+
+        p.settings(
+          libraryDependencies ++= Seq(
+            "com.disneystreaming" %% "weaver-cats"       % weaverVersion % Test,
+            "com.disneystreaming" %% "weaver-scalacheck" % weaverVersion % Test
+          )
+        )
+      }
     }
   }
 }
--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -1,38 +1,6 @@
 import sbt.Keys.*
 import sbt.*
 
-/**
-  * Automatically enriches projects with the following settings (despite the word "override").
-  */
 object ProjectPlugin extends AutoPlugin {
-
-  /**
-    * Defines what members will be imported to the `build.sbt` scope.
-    */
-  val autoImport = ThingsToAutoImport
-
-  /**
-    * Thus plug-in will automatically be enabled; it has no requirements.
-    */
-  override def trigger: PluginTrigger = AllRequirements
-
-  object ThingsToAutoImport {
-    implicit class ProjectOps(p: Project) {
-      def withCats: Project =
-        p
-          .settings(libraryDependencies += "org.typelevel" %% "cats-core" % "2.13.0")
-
-      def withTesting: Project = {
-        val weaverVersion =
-          "0.8.4"
-
-        p.settings(
-          libraryDependencies ++= Seq(
-            "com.disneystreaming" %% "weaver-cats"       % weaverVersion % Test,
-            "com.disneystreaming" %% "weaver-scalacheck" % weaverVersion % Test
-          )
-        )
-      }
-    }
-  }
+  override def trigger = noTrigger
 }
